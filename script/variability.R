5141c179b5b9ecf39b2a1294623c9970236db449
ame-gagliardi <ame.gagliardi@gmail.com>
2020-10-21 22:45:28 +0200

    update

--- script/variability.R
+++ script/variability.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(DESeq2)
 
 df <- as.data.frame(readRDS("data/clinical/de_merged_cleaned.rds"))
 rownames(df) <- df$id
@@ -15,47 +17,60 @@ levName <- c(levels(df$age_cat), levels(df$sex), levels(df$alcool), levels(df$wi
 
 levName[c(9,15,16)] <- c("w.abst", "cigs.nev", "cigs.form")
 
-data <- as.data.frame(matrix(NA, nrow = 3524, ncol = 30, dimnames = list(c(colnames(norm)),
+dataMean <- as.data.frame(matrix(NA, nrow = 3524, ncol = 30, dimnames = list(c(colnames(norm)),
                                                                         c(levName))))
+covars <- c("age_cat", "sex", "alcool", "wine_consumption", "smoke", "ncigs", "phys_act", "bmi_cat", "coffee_cat", "mestr_now")
 
-var <- c("ncigs")
-cat <- levels(df$ncigs)
+print(Sys.time())
+for(k in 1:length(covars)){
+  var <- covars[k]
+  cat <- levels(df[,var])
 
-for(z in 1:length(cat)){
-  for(i in 1:length(rownames(data))){
-    df[,18] <- norm[,i]
-    colnames(df)[18] <- colnames(norm)[i]
-    db <- df %>% 
-      dplyr::filter(ncigs == cat[z])
-    data[i,cat[z]] <- round(mean(db[,18], na.rm = TRUE), digits = 3)
+  for(z in 1:length(cat)){
+    for(i in 1:length(rownames(dataMean))){
+      df[,18] <- norm[,i]
+      colnames(df)[18] <- colnames(norm)[i]
+      db <- df 
+      db <- db %>% 
+        dplyr::filter(db[,var] == cat[z])
+       dataMean[i,cat[z]] <- round(mean(db[,18], na.rm = TRUE), digits = 3)
+    }
   }
 }
+print(Sys.time())
 
 
 
 
-data[,"w.abst"] <- NULL
-data[,"cigs.nev"] <- NULL
-data[,"cigs.form"] <- NULL
+dataMean[,"w.abst"] <- NULL
+dataMean[,"cigs.nev"] <- NULL
+dataMean[,"cigs.form"] <- NULL
 
 
-saveRDS(data, file = "data/miRNA_mean.rds")
+saveRDS(dataMean, file = "data/miRNA_mean.rds")
 
 
 
 
-median <- as.data.frame(matrix(NA, nrow = 3524, ncol = 30, dimnames = list(c(colnames(norm)),
+dataMedian <- as.data.frame(matrix(NA, nrow = 3524, ncol = 30, dimnames = list(c(colnames(norm)),
                                                                          c(levName))))
 
-var <- c("age_cat")
-cat <- levels(df$age_cat)
-
-for(z in 1:length(cat)){
-  for(i in 1:length(rownames(median))){
-    df[,18] <- norm[,i]
-    colnames(df)[18] <- colnames(norm)[i]
-    db <- df %>% 
-      dplyr::filter(age_cat == cat[z])
-    median[i,cat[z]] <- round(median(db[,18], na.rm = TRUE), digits = 3)
+print(Sys.time())
+for(k in 1:length(covars)){
+  var <- covars[k]
+  cat <- levels(df[,var])
+  
+  for(z in 1:length(cat)){
+    for(i in 1:length(rownames(dataMedian))){
+      df[,18] <- norm[,i]
+      colnames(df)[18] <- colnames(norm)[i]
+      db <- df 
+      db <- db %>% 
+        dplyr::filter(db[,var] == cat[z])
+      dataMedian[i,cat[z]] <- round(median(db[,18], na.rm = TRUE), digits = 3)
+    }
   }
 }
+print(Sys.time())
+
+saveRDS(dataMedian, file = "data/miRNA_median.rds")
