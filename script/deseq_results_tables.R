3f8a8f217fe2e38643e5a6d8b3dcef0eddf9807b
ame-gagliardi <ame.gagliardi@gmail.com>
2020-11-02 15:17:25 +0100

    update

--- script/deseq_results_tables.R
+++ script/deseq_results_tables.R
@@ -1,50 +1,64 @@
-library(tidyverse)
-library(DESeq2)
+source("C:/Users/amedeo/Desktop/R_Projects/general_script/useful_functions.R")
 
-refCov <- c("age_terz")
-refDb <- c("male")
+refCov <- c("bmi_cat")                   # Variable to study
+refDb <- c("all")                        # Dataset with the variable
+cts <- c("normalized")                   # Count matrix (normalized or raw)
+result.path <- c("results/prova")        # Path to save results
+dir.create(path = result.path, showWarnings = FALSE)
 
 ## Dataset load
-df <- read.delim(paste0("data/clinical/de_",refDb,"_merged_cleaned.txt"))
-df[,refCov] <- as.factor(df[,refCov])
-levName <- levels(df[,refCov])
+df <- readRDS(paste0("data/clinical/de_",refDb,"_merged_cleaned.rds"))
+cts <- readRDS(paste0("data/ngs/",cts,"_counts.rds"))
+
 ## DEseq analysis results
 dds <- readRDS(paste0("results/full_model/dds/",refCov,".rds"))
 results(dds)
-## Mean and median loading
-mean <- readRDS("data/miRNA_mean.rds")
-mean$ID <- rownames(mean)
-median <- readRDS("data/miRNA_median.rds")
-median$ID <- rownames(median)
 
 ## Insert here case-by-case modification to the datasets - DELETE BEFORE CLOSING
+levels(df$bmi_cat) <- c("normal", "over", "over", NA)
+levels(df$bmi_cat) <- droplevels(df$bmi_cat)
 ##
 
 ## Comparison
+levName <- covarLevels(refCov, df)
 comp <- combn(levName, m=2)
 check <- rownames(results(dds))
+##
 
 for(i in 1:ncol(comp)){
-  cov1 <- comp[1,i] # Confronti delle variabili nelle colonne di comp
+  cov1 <- comp[1,i]                                # Confronti delle variabili nelle colonne di comp
   cov2 <- comp[2,i]
+  mean <- miRNA_average_class(refCov, df, cts)     # Creo una matrice con le media dei miRNA per ogni livello della variabile
+  mean$ID <- rownames(mean)
+  median <- miRNA_median_class(refCov, df, cts)    # Creo una matrice con le mediane dei miRNA per ogni livello della variabile
+  median$ID <- rownames(median)
   
-  keep <- c(levName, "ID") # Select the mean and median of the refCov
-  mean <- mean[,keep]
-  median <- median[,keep]
   if(all.equal(check, rownames(mean))){
-    res <- results(dds, contrast = c(refCov, cov2, cov1))
-    res[,"ID"] <- rownames(res)
-    res[,"comparison"] <- paste0(cov2, "_vs_", cov1)
-    res[,"treshold"] <- res$padj <0.05
-    res <- as.data.frame(res)
-    res <- merge(res, mean, by = "ID")
-    res <- merge(res, median, by = "ID")
-    write.table(res, file = paste0("results/full_model/tables/DE_results_", cov2,"_vs_", cov1,".txt", sep = ""), sep = "\t", quote = F, row.names = F)
+    res <- results(dds, contrast = c(refCov, cov2, cov1)) # Carico il dds con i risultati del contrast specificato
+    res[,"ID"] <- rownames(res)                           # Aggiungo il nome dei miRNA
+    res[,"comparison"] <- paste0(cov2, "_vs_", cov1)      # Aggiungo il nome del confronto
+    res[,"treshold"] <- res$padj <0.05                    # Aggiungo un V/F sul treshold di significatività
+    res <- as.data.frame(res)                
+    ctrl <- all.equal(rownames(mean), rownames(median))
+    ctrl2 <- all.equal(rownames(mean), rownames(dds))
+      if(ctrl == TRUE & ctrl2 == TRUE){
+        res <- merge(res, mean, by = "ID")                # Merge della matrice dei risultati con media e mediana
+        res <- merge(res, median, by = "ID")
+      }else{
+        print("Rownames non equal: res and mean not merged")
+      }
+  
+  dir.create(paste0(result.path, "/tables/"), showWarnings = FALSE) # Creo la cartella dove salvare i risultati
+  write.table(res, file = paste0(result.path,"/tables/DE_results_", cov2,"_vs_", cov1,".txt", sep = ""), sep = "\t", quote = F, row.names = F)
+  rm(cov1, cov2, ctrl, ctrl2, mean, median, res)
   }else{
     print("Rownames no equal")
   }
-    
 }
 
 rm(list=ls())
+
+
+
+
 
