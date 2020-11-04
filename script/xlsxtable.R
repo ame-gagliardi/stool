9e4541d3119b9626378712545c2c16f53932541d
ame-gagliardi <ame.gagliardi@gmail.com>
2020-11-04 14:17:44 +0100

    update

--- script/xlsxtable.R
+++ script/xlsxtable.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(openxlsx)
 
 # Parameters
-refDB <- "male"
+refDB <- "female"
 result.path <- paste0("results/by_sex/", refDB)
 
 
 #Script start
 covars <- c("age_cat", "age_terz", "alcool", "alcool_28", "alcool_drinker", 
-            "bmi_cat", "coffee_cat", "coffee_drinker", "phys_act", "smoke", "wine_consumption")
+            "bmi_cat", "coffee_cat", "coffee_drinker", "phys_act", "smoke", "wine_consumption", "ncigs")
 
 for(i in 1:length(covars)){
   refCov <- covars[i]
@@ -39,17 +39,9 @@ for(i in 1:length(covars)){
   addWorksheet(wb, covars[i])
 }
 
-writeData(wb, "age_cat", age_cat_db, startRow = 1, startCol = 1)
-writeData(wb, "age_terz", age_terz_db, startRow = 1, startCol = 1)
-writeData(wb, "alcool", alcool_db, startRow = 1, startCol = 1)
-writeData(wb, "alcool_28", alcool_28_db, startRow = 1, startCol = 1)
-writeData(wb, "alcool_drinker", alcool_drinker_db, startRow = 1, startCol = 1)
-writeData(wb, "bmi_cat", bmi_cat_db, startRow = 1, startCol = 1)
-writeData(wb, "coffee_cat", coffee_cat_db, startRow = 1, startCol = 1)
-writeData(wb, "coffee_drinker", coffee_drinker_db, startRow = 1, startCol = 1)
-writeData(wb, "phys_act", phys_act_db, startRow = 1, startCol = 1)
-writeData(wb, "smoke", smoke_db, startRow = 1, startCol = 1)
-writeData(wb, "wine_consumption", wine_consumption_db, startRow = 1, startCol = 1)
-writeData(wb, "ncigs", ncigs_db, startRow = 1, startCol = 1)
+for(i in 1:length(covars)){
+  writeData(wb, covars[i], get(paste0(covars[i],"_db")), startRow = 2, startCol = 1)
+}
 
-saveWorkbook(wb, file = "results/male_results.xlsx", overwrite = TRUE)
+
+saveWorkbook(wb, file = "results/female_results.xlsx", overwrite = TRUE)
