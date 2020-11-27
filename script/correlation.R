71a76795e8a5d17da38fb6008cf0a4e9e131b474
ame-gagliardi <ame.gagliardi@gmail.com>
2020-11-27 16:54:16 +0100

    update

--- script/correlation.R
+++ script/correlation.R
@@ -1,89 +1,45 @@
-library(tidyverse)
+source("C:/Users/amedeo/Desktop/R_Projects/general_script/useful_functions.R")
 library(ggpubr)
-library(DESeq2)
 
-## Data load
+# Data loading
 
-cts <- readRDS("data/ngs/merged_harmonized_converted.rds")
-
-df <- readRDS("data/clinical/de_all_merged_cleaned.rds")
+df <- readRDS("data/clinical/de_all_merged_cleaned.rds") %>% 
+  dplyr::filter(sex == "male")
 rownames(df) <- df$id
 
-i <- intersect(rownames(df), colnames(cts))
+cts <- readRDS("data/ngs/raw_counts.rds")
+norm <- readRDS("data/ngs/normalized_counts.rds")
+
+i <- intersect(rownames(df), colnames(norm))
 df <- df[i,]
-cts <- cts[,i]
-all.equal(rownames(df), colnames(cts))
+norm <- norm[,i]
 
-## Testing normal distribution of BMI and Age
 
-# Age
-ggdensity(df$age,
-          main = c("Density plot of age distribution"),
-          xlab = c("Age (years)"))
 
-shapiro.test(df$age) # p.value is lower than 0.05, age is not normally ditrubuted
+# Age and BMI correlation
+t.norm <- as.data.frame(t(norm))
 
-ggpubr::ggqqplot(df$age, col = "steelblue", size = 1, title = c("qqPlot of age"))
+db <- df %>% 
+  dplyr::select(id, age, bmi)
 
-## Correlation test for age
-cts <- as.data.frame(t(cts))
-corrAge <- as.data.frame(matrix(data = NA, nrow = length(rownames(cts)), ncol = 3))
-colnames(corrAge) <- c("miRNA", "kendall_correlation", "pvalue")
+corrMat <- as.data.frame(matrix(NA, nrow = length(colnames(t.norm)), ncol = 7))
+colnames(corrMat) <- c("miRNA", "age", "age.p", "age.fdr", "bmi", "bmi.p", "bmi.fdr")
 
-for(i in 1:length(colnames(cts))){
-  df[,22] <- cts[,i]
-  colnames(df)[22] <- colnames(cts)[i]
-  resKen <- cor.test(df$age, df[,last(colnames(df))], method = "kendall")
-  corrAge[i,1] <- colnames(cts)[i]
-  corrAge[i,2] <- resKen$estimate
-  corrAge[i,3] <- resKen$p.value
+
+for(i in 1:length(colnames(t.norm))){
+  
+  db[,4] <- t.norm[,i]
+  ken.age <- cor.test(db$age, db$V4, method = "kendall")
+  ken.bmi <- cor.test(db$bmi, db$V4, method = "kendall")
+  corrMat[i, "miRNA"] <- colnames(t.norm)[i]
+  corrMat[i, "age"] <- round(ken.age$estimate, 3)
+  corrMat[i, "age.p"] <- round(ken.age$p.value, 3)
+  corrMat[i, "bmi"] <- round(ken.bmi$estimate, 3)
+  corrMat[i, "bmi.p"] <- round(ken.bmi$p.value, 3)
 }
 
-corrAge <- merge(corrAge, sig_tot, by = "miRNA")
-# Bmi
-# I'll delete the underweight category
-df <- readRDS("data/clinical/de_all_merged_cleaned.rds")
-rownames(df) <- df$id
-rm <- which(df$bmi_cat == "underweight")
-df <- df[-rm,]
-
-cts <- readRDS("data/ngs/merged_harmonized_converted.rds") %>% 
-  dplyr::select(-VOV114)
-
-i <- intersect(rownames(df), colnames(cts))
-df <- df[i,]
-cts <- cts[,i]
-all.equal(rownames(df), colnames(cts))
-
-ggdensity(df$bmi,
-          main = c("Density plot of bmi distribution"),
-          xlab = c("BMI"))
-
-shapiro.test(df$bmi) # p.value is lower than 0.05, bmi is not normally distributed
-
-ggpubr::ggqqplot(df$bmi, col = "steelblue", size = 1, title = c("qqPlot of age"))
-
-cts <- as.data.frame(t(cts))
-
-i <- intersect(rownames(cts), rownames(df))
-cts <- cts[i,]
-df <- df[i,]
-all.equal(rownames(df), rownames(cts))
-
-corrBMI <- as.data.frame(matrix(data = NA, nrow = length(colnames(cts)), ncol = 3))
-colnames(corrBMI) <- c("miRNA", "kendall_correlation", "pvalue")
-
-for(i in 1:length(colnames(cts))){
-  df[,22] <- cts[,i]
-  colnames(df)[22] <- colnames(cts)[i]
-  resKen <- cor.test(df$bmi, df[,22], method = "kendall")
-  corrBMI[i,1] <- colnames(cts)[i]
-  corrBMI[i,2] <- resKen$estimate
-  corrBMI[i,3] <- resKen$p.value
-}
-
-corrBMI <-  merge(corrBMI, sig_tot, by = "miRNA")
+corrMat$age.fdr <- p.adjust(corrMat$age.p, method = "fdr")
+corrMat$bmi.fdr <- p.adjust(corrMat$bmi.p, method = "fdr")
 
 
-write.table(corrAge, file = "results/age_correlation.txt", sep = "\t", row.names = FALSE, quote = FALSE)
-write.table(corrBMI, file = "results/bmi_correlation.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(corrMat, file = c("results/age_bmi_correlation_male.txt"), sep = "\t", row.names = FALSE, quote = FALSE)
