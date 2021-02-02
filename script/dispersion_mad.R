39714ab0d5de103c6e0ed8acb8c8e94542aeac5a
ame-gagliardi <ame.gagliardi@gmail.com>
2021-02-02 16:52:21 +0100

    update

--- script/dispersion_mad.R
+++ script/dispersion_mad.R
@@ -39,54 +39,52 @@ tmp <- tmp[order(tmp$mad),] # Order by MAD
 tmp[,"order"] <- seq(1:length(rownames(tmp))) 
 tmp[,"mad_position"] <- rep(c("top","middle", "bottom"), times = c(20,279,13)) # legend of MAD position
 tmp[,"position_legend"] <- ifelse(tmp$mad_position == "top" | tmp$mad_position == "bottom", TRUE, FALSE) # label for plot
-tmp[,"coefficient"] <- tmp$mad/tmp$median # MAD/Median ratio
-tmp <- tmp[order(tmp$coefficient),] # Order by MAD/median ratio
-tmp[,"coefficient_order"] <- seq(1:length(rownames(tmp))) 
-tmp[,"coefficient_position"] <- rep(c("top", "middle", "bottom"), times = c(20,279,13)) # legend of MAD/Median ratio
-tmp[,"coefficient_label"] <- ifelse(tmp$coefficient_position == "top" | tmp$coefficient_position == "bottom", TRUE, FALSE) # label for plot
+tmp[,"cv"] <- tmp$mad/tmp$median # MAD/Median ratio
+tmp <- tmp[order(tmp$cv),] # Order by MAD/median ratio
+tmp[,"cv_order"] <- seq(1:length(rownames(tmp))) 
+tmp[,"cv_position"] <- rep(c("top", "middle", "bottom"), times = c(20,279,13)) # legend of MAD/Median ratio
+tmp[,"cv_label"] <- ifelse(tmp$cv_position == "top" | tmp$cv_position == "bottom", TRUE, FALSE) # label for plot
 rownames(tmp) <- tmp$miRNA
+
+tmp[,"top"] <- ifelse(tmp$miRNA == "hsa-miR-647-3p:Novel" | tmp$miRNA == "hsa-miR-6075" | 
+                      tmp$miRNA == "hsa-miR-3125" | tmp$miRNA == "hsa-miR-320e-5p:Novel" |
+                      tmp$miRNA == "hsa-miR-1246-3p" | tmp$miRNA == "hsa-miR-607" | tmp$miRNA == "hsa-miR-1302", TRUE, FALSE)
+
+tmp[,"italic"] <- ifelse(tmp$top == TRUE & str_detect(tmp$miRNA, ":Novel"),1,FALSE)
+
+toellipse <- tmp[c(1,2,3,5),]
+
 # Plot
 
-p1 <- ggplot(tmp, aes(x=log(mad), y=log(median))) +
-  geom_point(aes(color=mad_position)) +
-  ylab(bquote(~log[10]~ "(median)")) +
-  xlab(bquote(~log[10]~ "(median absolute deviation)")) +
-  theme_classic() +
-  scale_color_branded() +
-  geom_text_repel(aes(label = ifelse(position_legend == TRUE, miRNA, "")), size = 2.5)
+top10title <- "Most stable miRNAs"
+top10 <- as.data.frame(matrix(NA, nrow = 10, ncol = 2))
+top10[,1] <- tmp$miRNA[1:10]
+t
+bottom10title <- "Most unstable miRNAs"
+bottom10 <- "hsa-miR-3619-3p \n hsa-miR-378i \n hsa-miR-4254-5p \n hsa-miR-548d-5p \n hsa-miR-371a-3p \n hsa-miR-1203 \n hsa-miR-4739-3p \n hsa-miR-646 \n hsa-miR-200c-3p \n hsa-miR-645"
 
-p2 <- ggplot(tmp, aes(x=log(mad), y=log(median))) +
-  geom_point(aes(color=mad_position)) +
+p <- ggplot(tmp, aes(x=cv, y=log(median))) +
+  geom_point(aes(color=top)) +
   ylab(bquote(~log[10]~ "(median)")) +
-  xlab(bquote(~log[10]~ "(median absolute deviation)")) +
-  theme_classic() +
-  scale_color_branded()
-
-p3 <- ggplot(tmp, aes(x=coefficient, y=median)) +
-  geom_point(aes(color=coefficient_position)) +
-  ylab("Median") +
   xlab(expression(frac(MAD,Median), cex = 1.5)) +
   theme_classic() +
-  scale_color_branded() +
-  geom_text_repel(aes(label = ifelse(coefficient_label == TRUE, miRNA, "")), size = 2.5)
-
-p4 <- ggplot(tmp, aes(x=coefficient, y=median)) +
-  geom_point(aes(color=coefficient_position)) +
-  ylab("Median") +
-  xlab(expression(frac(MAD,Median), cex = 1.5)) +
-  theme_classic() +
-  scale_color_branded()
+  annotate("text", x=1.2, y=7.5, label = top10title, fontface = 2) +
+  annotate("text", x=1.2, y=6.8, label = prova, size = 3, parse = TRUE) +
+  annotate("text", x=1.3, y=7.5, label = bottom10title, fontface = 2) +
+  annotate("text", x=1.3, y=6.8, label = bottom10, size = 3, fontface = 3) +
+  geom_text_repel(aes(label=ifelse(italic==1, str_remove_all(miRNA, ":Novel"), "")), size = 3.5, fontface="bold.italic") +
+  geom_text_repel(aes(label=ifelse(italic==0 & top == TRUE, miRNA, "")), size=3.5, fontface="bold") +
+  guides(color=FALSE)
 
 
+prova <- bquote(atop("italic('hsa-miR-647-3p')", "hsa-miR-6075"))
 
+p
 
-
-ggsave(p1, filename = "mad_plot_names.png", width = 30, height = 30, units = "cm", dpi = 300)
-ggsave(p2, filename = "mad_plot_no_names.png", width = 30, height = 30, units = "cm", dpi = 300)
-ggsave(p3, filename = "mad_median_ratio_names.png", width = 30, height = 30, units = "cm", dpi = 300)
-ggsave(p4, filename = "mad_median_ratio_no_names.png", width = 30, height = 30, units = "cm", dpi = 300)
+ggsave(p, filename = "mad_median_log.png", width = 30, height = 30, units = "cm", dpi = 300)
 
 
 
-
+prova <- c(atop("italic('hsa-miR-647-3p')", 
+                "hsa-miR-6075"))
 #####
